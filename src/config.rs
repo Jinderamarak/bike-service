a9e1bf5e75b537a854d45365857ba9ae2e1f21a6
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2024-02-07 00:10:56 +0100

    chore: change default port and formatting

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use clap::Parser;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 const IPV4_ALL: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
 
@@ -8,9 +8,21 @@ const IPV4_ALL: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
 pub struct Configuration {
     #[arg(short, long, env = "BIKE_ADDRESS", default_value_t = IPV4_ALL, help = "IP address for the server to listen on")]
     pub address: IpAddr,
-    #[arg(short, long, env = "BIKE_PORT", default_value_t = 3000, help = "Port for the server to listen on")]
+    #[arg(
+        short,
+        long,
+        env = "BIKE_PORT",
+        default_value_t = 8080,
+        help = "Port for the server to listen on"
+    )]
     pub port: u16,
-    #[arg(short, long, env = "DATABASE_URL", default_value = "sqlite:./data.db?mode=rwc", help = "SQLite database url")]
+    #[arg(
+        short,
+        long,
+        env = "DATABASE_URL",
+        default_value = "sqlite:./data.db?mode=rwc",
+        help = "SQLite database url"
+    )]
     pub database_url: String,
 }
 
