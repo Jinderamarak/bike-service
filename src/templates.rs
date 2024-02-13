bc9919e2a324bf798bfa57a0a2cba2266f9db7da
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2024-02-13 23:37:30 +0100

    feat: format floating distance without trailing zeros

--- src/templates.rs
+++ src/templates.rs
@@ -36,3 +36,13 @@ pub struct RideEditTemplate {
 pub struct RideTotalTemplate {
     pub total: f64,
 }
+
+mod filters {
+    use std::fmt::Display;
+
+    pub fn removesuffix<T: Display>(s: T, suffix: &str) -> ::askama::Result<String> {
+        let s = s.to_string();
+        let trimmed = s.trim_end_matches(suffix).to_string();
+        Ok(trimmed)
+    }
+}
