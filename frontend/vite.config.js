a98f37738a5adb4b2a1addd47dd3920eaed70943
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2024-06-06 16:04:10 +0200

    refactor: vite config improvements

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,66 +3,55 @@ import { buildSync } from "esbuild";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    define: {
+export default defineConfig(() => {
+    const define = {
         APP_VERSION: JSON.stringify(process.env.npm_package_version),
-    },
-    plugins: [
-        react(),
-        {
-            apply: "build",
-            enforce: "post",
-            transformIndexHtml() {
-                buildSync({
-                    minify: true,
-                    bundle: true,
-                    entryPoints: ["worker/worker.js"],
-                    outfile: "dist/worker.js",
-                    define: {
-                        APP_VERSION: JSON.stringify(
-                            process.env.npm_package_version
-                        ),
-                    },
-                });
-            },
-        },
-        {
-            apply: "serve",
-            enforce: "post",
-            configureServer(server) {
-                server.middlewares.use("/worker.js", (req, res, next) => {
-                    res.writeHead(200, {
-                        "Content-Type": "application/javascript",
+    };
+    return {
+        define,
+        plugins: [
+            react(),
+            {
+                transformIndexHtml() {
+                    buildSync({
+                        minify: true,
+                        bundle: true,
+                        entryPoints: ["worker/worker.js"],
+                        outfile: "dist/worker.js",
+                        define,
                     });
-                    res.end(
-                        buildSync({
-                            minify: false,
-                            bundle: true,
-                            entryPoints: ["worker/worker.js"],
-                            write: false,
-                            define: {
-                                APP_VERSION: JSON.stringify(
-                                    process.env.npm_package_version
-                                ),
-                            },
-                        }).outputFiles[0].text
-                    );
-                });
+                },
+                configureServer(server) {
+                    server.middlewares.use("/worker.js", (req, res, next) => {
+                        res.writeHead(200, {
+                            "Content-Type": "application/javascript",
+                        });
+                        res.end(
+                            buildSync({
+                                minify: false,
+                                bundle: true,
+                                entryPoints: ["worker/worker.js"],
+                                write: false,
+                                define,
+                            }).outputFiles[0].text
+                        );
+                    });
+                },
+            },
+        ],
+        build: {
+            rollupOptions: {
+                output: {
+                    entryFileNames: "assets/[name].js",
+                    chunkFileNames: "assets/[name].js",
+                    assetFileNames: "assets/[name].[ext]",
+                },
             },
         },
-    ],
-    build: {
-        rollupOptions: {
-            output: {
-                entryFileNames: "assets/[name].js",
-                chunkFileNames: "assets/[name].js",
-                assetFileNames: "assets/[name].[ext]",
+        server: {
+            proxy: {
+                "/api": "http://localhost:8080",
             },
         },
-    },
-    server: {
-        proxy: {
-            "/api": "http://localhost:8080",
-        },
-    },
+    };
 });
