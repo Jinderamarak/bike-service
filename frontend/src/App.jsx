1b9e617946c915cedacb2c4f215792bf68534be0
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2024-07-11 10:00:09 +0200

    feat: improved app loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,35 @@
-import React from "react";
-import { Stack } from "@mantine/core";
+import React, { useEffect, useState } from "react";
+import { Stack, Transition } from "@mantine/core";
 import Navigation from "./components/navigation/Navigation.jsx";
 import BikesPage from "./pages/bikes/BikesPage.jsx";
 import StatsPage from "./pages/stats/StatsPage.jsx";
 import RidesPage from "./pages/rides/RidesPage.jsx";
 import DataPage from "./pages/data/DataPage.jsx";
-import { Routes, Route } from "react-router-dom";
-import { syncLocalStorate as syncLocalStorage } from "./data/persistentAtoms.js";
-import { syncNetworkStatus } from "./data/useNetworkStatus.jsx";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
-    syncLocalStorage();
-    syncNetworkStatus();
+    const [transition, setTransition] = useState(false);
+
+    useEffect(() => {
+        setTransition(true);
+    }, []);
 
     return (
-        <Stack p="md" gap="md">
-            <Navigation />
-            <Routes>
-                <Route index element={<RidesPage />} />
-                <Route path="/bikes" element={<BikesPage />} />
-                <Route path="/stats" element={<StatsPage />} />
-                <Route path="/data" element={<DataPage />} />
-            </Routes>
-        </Stack>
+        <BrowserRouter>
+            <Transition mounted={transition} transition="fade">
+                {(styles) => (
+                    <Stack p="md" gap="md" style={{ ...styles }}>
+                        <Navigation />
+                        <Routes>
+                            <Route index element={<RidesPage />} />
+                            <Route path="/bikes" element={<BikesPage />} />
+                            <Route path="/stats" element={<StatsPage />} />
+                            <Route path="/data" element={<DataPage />} />
+                        </Routes>
+                    </Stack>
+                )}
+            </Transition>
+        </BrowserRouter>
     );
 }
 
---
+++ frontend/src/AppEntry.jsx
@@ -0,0 +1,42 @@
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import { Center, Group, Loader, Stack, Title, Transition } from "@mantine/core";
+import { useLocalStorageSync } from "./data/persistentAtoms.js";
+import { useNetworkStatusSync } from "./data/useNetworkStatus.jsx";
+
+const LazyApp = lazy(() => import("./App.jsx"));
+
+function Loading() {
+    const [transition, setTransition] = useState(false);
+
+    useEffect(() => {
+        setTransition(true);
+    }, []);
+
+    return (
+        <Transition mounted={transition} transition="fade">
+            {(styles) => (
+                <Center h="100%" style={{ ...styles }}>
+                    <Stack>
+                        <Group justify="center">
+                            <Loader />
+                        </Group>
+                        <Title>Loading</Title>
+                    </Stack>
+                </Center>
+            )}
+        </Transition>
+    );
+}
+
+function AppEntry() {
+    useLocalStorageSync();
+    useNetworkStatusSync();
+
+    return (
+        <Suspense fallback={<Loading />}>
+            <LazyApp />
+        </Suspense>
+    );
+}
+
+export default AppEntry;
